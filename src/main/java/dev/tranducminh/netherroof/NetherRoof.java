5dcc93ecdfbde28d632220d7d2283422907dcf86
xGinko <rachel.audrey@mail.de>
2024-06-25 20:46:47 +0200

    optimize imports

--- src/main/java/dev/tranducminh/netherroof/NetherRoof.java
+++ src/main/java/dev/tranducminh/netherroof/NetherRoof.java
@@ -1,6 +1,5 @@
 package dev.tranducminh.netherroof;
 
-import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
@@ -9,7 +8,6 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.concurrent.TimeUnit;
 
